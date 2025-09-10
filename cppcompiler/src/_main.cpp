13e486be13595a0a4b7d933b96454262dd2c23da
F100cTomas <145491617+F100cTomas@users.noreply.github.com>
2025-09-10 17:36:39 +0200

    Improved linux compilation + minor formatting changes

--- cppcompiler/src/_main.cpp
+++ cppcompiler/src/_main.cpp
@@ -1,20 +1,19 @@
+#include "CodeGenerator/_codegenerator.hpp"
 #include "Lexer/_lexer.hpp"
 #include "Parser/_parser.hpp"
-#include "CodeGenerator/_codegenerator.hpp"
 #include <fstream>
 #include <iostream>
 #include <sstream>
-
 int main() {
 	std::ifstream     file{"code"};
 	std::stringstream str;
 	str << file.rdbuf();
 	std::vector<Lexer::Token> tokens = Lexer::run(str.str().c_str());
 	for (const Lexer::Token& token: tokens)
-	  std::cout << token << ' ';
+		std::cout << token << ' ';
 	std::cout << '\n';
 	Parser::AST ast = Parser::run(tokens);
 	std::cout << ast;
 	std::unique_ptr<llvm::Module> module = CodeGenerator::run(ast);
-	module->print(llvm::outs(), nullptr);
+	// module->print(llvm::outs(), nullptr);
 }
--- cppcompiler/src/builtins.hpp
+++ cppcompiler/src/builtins.hpp
@@ -9,7 +9,7 @@ namespace {
 constexpr uint64_t hashfn(const char* str) {
 	uint64_t hash = 14695981039346656037U;
 	while (*str) {
-		hash ^= (uint8_t)*str++;
+		hash ^= static_cast<uint8_t>(*str++);
 		hash *= 1099511628211U;
 	}
 	return hash;
@@ -17,50 +17,40 @@ constexpr uint64_t hashfn(const char* str) {
 } // namespace
 constexpr std::optional<uint32_t> infix_operator_precedence(const Lexer::Token& token) {
 	switch (hashfn(token)) {
-	case hashfn("."):
-		return 1;
+	case hashfn("."): return 1;
 	case hashfn(""):
 	case hashfn("*"):
 	case hashfn("/"):
-	case hashfn("%"):
-		return 3;
+	case hashfn("%"): return 3;
 	case hashfn("+"):
-	case hashfn("-"):
-		return 5;
-	case hashfn("^"):
-		return 7;
-	default:
-		return {};
+	case hashfn("-"): return 5;
+	case hashfn("^"): return 7;
+	default: return {};
 	}
 }
 constexpr std::optional<uint32_t> prefix_operator_precedence(const Lexer::Token& token) {
 	switch (hashfn(token)) {
 	case hashfn("+"):
-	case hashfn("-"):
-		return 2;
-	default:
-		return {};
+	case hashfn("-"): return 2;
+	default: return {};
 	}
 }
 constexpr std::optional<uint32_t> postfix_operator_precedence(const Lexer::Token& token) {
 	switch (hashfn(token)) {
-	default:
-		return {};
+	default: return {};
 	}
 }
 constexpr bool is_operator(const Lexer::Token& token) {
-	return infix_operator_precedence(token).has_value() || prefix_operator_precedence(token).has_value() ||
-	       postfix_operator_precedence(token).has_value();
+	return infix_operator_precedence(token).has_value() || prefix_operator_precedence(token).has_value()
+	       || postfix_operator_precedence(token).has_value();
 }
 constexpr bool is_keyword(const Lexer::Token& token) {
 	switch (hashfn(token)) {
 	case hashfn("("):
 	case hashfn("{"):
 	case hashfn("if"):
-	case hashfn("fn"):
-		return true;
-	default:
-		return false;
+	case hashfn("fn"): return true;
+	default: return false;
 	}
 }
 } // namespace Operators
