ac1f34a7650fdc2677ef05fdaae328179ebd2d73
F100cTomas <145491617+F100cTomas@users.noreply.github.com>
2025-09-28 17:13:04 +0200

    SymbolTable progress

--- cppcompiler/src/Parser/symboltable.cpp
+++ cppcompiler/src/Parser/symboltable.cpp
@@ -13,4 +13,37 @@ constexpr uint64_t hashfn(const char* str) {
 } // namespace
 SymbolTable::SymbolTable(SymbolTable* upper) : _upper_level(upper) {}
 SymbolTable::~SymbolTable() {}
+void SymbolTable::insert(const Lexer::Token& index, ASTNode* value) {
+	uint8_t                  i   = hashfn(index) % 16;
+	std::vector<TableValue>& row = _data[i];
+	for (TableValue& item: row) {
+		if (item._symbol == index.get()) {
+			item._data = value;
+			return;
+		}
+	}
+	row.emplace_back(index, value);
+}
+ASTNode* SymbolTable::get(const Lexer::Token& index) const {
+	uint8_t                        i   = hashfn(index) % 16;
+	const std::vector<TableValue>& row = _data[i];
+	for (const TableValue& item: row) {
+		if (item._symbol == index.get())
+			return item._data;
+	}
+	if (_upper_level == nullptr)
+		return nullptr;
+	return _upper_level->get(index);
+}
+bool SymbolTable::is_registered(const Lexer::Token& index) const {
+	uint8_t                        i   = hashfn(index) % 16;
+	const std::vector<TableValue>& row = _data[i];
+	for (const TableValue& item: row) {
+		if (item._symbol == index.get())
+			return true;
+	}
+	if (_upper_level == nullptr)
+		return false;
+	return _upper_level->is_registered(index);
+}
 } // namespace Parser
--- cppcompiler/src/Parser/symboltable.hpp
+++ cppcompiler/src/Parser/symboltable.hpp
@@ -1,21 +1,24 @@
 #pragma once
 #include "../Lexer/_lexer.hpp"
 #include <vector>
-
 namespace Parser {
 struct ASTNode;
+struct TableValue {
+	Lexer::Token _symbol;
+	ASTNode* _data;
+	inline TableValue(Lexer::Token symbol, ASTNode* data) : _symbol(symbol), _data(data) {}
+};
 class SymbolTable {
-	struct TablePair {
-		Lexer::Token symbol;
-		ASTNode* value;
-	};
-private:
-	std::vector<TablePair> _data[16]{};
-	SymbolTable* _upper_level{nullptr};
+	std::vector<TableValue> _data[16]{};
+	SymbolTable*            _upper_level{nullptr};
+
 public:
 	SymbolTable(SymbolTable* upper);
 	~SymbolTable();
+
 public:
-	// void insert(const Lexer::Token& index, ASTNode* value);
+	void insert(const Lexer::Token& index, ASTNode* value);
+	ASTNode* get(const Lexer::Token& index) const;
+	bool is_registered(const Lexer::Token& index) const;
 };
-}
+} // namespace Parser
